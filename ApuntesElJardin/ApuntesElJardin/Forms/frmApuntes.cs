59199b353b1abe7c77cb1a69f20224a446a3b2e8
Rafa Cruz <jose.cruz23@ulatina.net>
2026-05-10 14:18:24 -0600

    Mejora en la busqueda despues de 'Pagar'

--- ApuntesElJardin/ApuntesElJardin/Forms/frmApuntes.cs
+++ ApuntesElJardin/ApuntesElJardin/Forms/frmApuntes.cs
@@ -75,6 +75,11 @@ namespace ApuntesElJardin.Forms
                     if (apuntesBL.PagarApunte(idApunte))
                     {
                         CargarGrid();
+                        if(txtNombre.Text.Length >= 3)
+                        {
+                            var filtrados = apuntesMiniMarket.FindAll(a => a.NombreCompleto.IndexOf(txtNombre.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                            gridApuntes.DataSource = filtrados;
+                        }
                     }
                     else
                     {
@@ -93,8 +98,8 @@ namespace ApuntesElJardin.Forms
             }
             else
             {
-                var empleadosFiltro = apuntesMiniMarket.Where(x => x.Trabajo.ToLower().Contains(cmbTrabajo.Text.ToLower())).ToList();
-                gridApuntes.DataSource = empleadosFiltro;
+                var filtrados = apuntesMiniMarket.FindAll(a => a.Trabajo.IndexOf(cmbTrabajo.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+                gridApuntes.DataSource = filtrados;
             }
         }
 
