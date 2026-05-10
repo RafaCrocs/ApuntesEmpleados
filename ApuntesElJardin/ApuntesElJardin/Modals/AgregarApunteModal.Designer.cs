f82f800465a21dece67d05af10732f7c3198b5f5
Rafa Cruz <jose.cruz23@ulatina.net>
2026-05-10 14:10:24 -0600

    +1 version

--- ApuntesElJardin/ApuntesElJardin/Modals/AgregarApunteModal.Designer.cs
+++ ApuntesElJardin/ApuntesElJardin/Modals/AgregarApunteModal.Designer.cs
@@ -202,7 +202,7 @@
             Controls.Add(txtNombre);
             Name = "AgregarApunteModal";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Apuntes Minimarket  v1.0.0";
+            Text = "Apuntes Minimarket  v1.0.1";
             ResumeLayout(false);
             PerformLayout();
         }
